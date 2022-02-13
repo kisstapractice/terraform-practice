2e3dd01b9129aca5f99f58baf7c9f87f5fca2a97
RDISOFTWARE\Administrator <support@rdisoftware.com>
2022-02-13 23:03:28 +0100

    modified variables practice

--- createinstance.tf
+++ createinstance.tf
@@ -1,9 +1,11 @@
 resource "aws_instance" "MyFirstInstance" {
-    count = 2
+    count = 1
     ami        = "ami-05803413c51f242b7"
     instance_type = "t2.micro"
 
     tags = {
       Name = "threedemoinstance-${count.index}"
     }
+
+    security_groups = "${var.Security_Group}"
 }
--- main.tf
+++
@@ -1,7 +0,0 @@
-## Provider's example
-
-# configure AWS provider
-provider "aws" {
-    region = "us-east-1"
-    version = "3.53.0"
-}
